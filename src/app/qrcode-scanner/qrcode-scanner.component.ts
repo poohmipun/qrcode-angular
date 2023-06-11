d00119a4c3e3377fcdf09f3370960872de7282f8
poohmipun <poohhmipundev@gmail.com>
2023-06-11 14:41:49 +0700

    edit to 30%

--- src/app/qrcode-scanner/qrcode-scanner.component.ts
+++ src/app/qrcode-scanner/qrcode-scanner.component.ts
@@ -38,7 +38,7 @@ export class QrCodeScannerComponent implements OnInit {
       const videoWidth = this.video.videoWidth;
       const videoHeight = this.video.videoHeight;
       const minDimension = Math.min(videoWidth, videoHeight);
-      const reduction = 0.38;
+      const reduction = 0.3;
       const scanRegionSize = minDimension * (1 - 2 * reduction);
       const x = (videoWidth - scanRegionSize) / 2;
       const y = (videoHeight - scanRegionSize) / 2;
